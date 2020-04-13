0c333bf9ec6cd897f6e891f6a99d42c1eea0cc1a
Nghia <nphan@fandm.edu>
2020-04-13 18:04:16 -0500

    First commit: Serve all the initial journals at “/api/journals”

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+
+const initialJournals = [
+	{
+		id: 1,
+		date: '2020-03-23',
+		todos: [
+			{
+				id: 1,
+				task: 'Learn React for two hours',
+				done: true
+			},
+			{
+				id: 2,
+				task: 'Learn to skate for one hour',
+				done: false
+			}
+		],
+		reflection: 'Today is good.',
+		book_summary: [
+			{
+				id: 1,
+				title: 'Catch-22',
+				chapter: 'The Texan',
+				summary: 'This chapter is very interesting'
+			}
+		],
+		words_of_today: [
+			{
+				id: 1,
+				word: 'Epiphany',
+				definition: 'A suddent moment of insight or realization.'
+			}
+		]
+	},
+	{
+		id: 2,
+		date: '2020-03-24',
+		todos: [
+			{
+				id: 1,
+				task: 'Mediatation for 5 minutes',
+				done: true
+			}
+		],
+		reflection: 'Today is fantastic.',
+		book_summary: [
+			{
+				id: 1,
+				title: 'Selfish Gene',
+				chapter: '12',
+				summary: 'This chapter is very insightful.'
+			}
+		]
+	}
+]
+
+app.get('/api/journals', (request, response) => {
+	response.json(initialJournals)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
