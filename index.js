351dc35acf2a66a0aee59912d886492d95e946e5
Nghia <nphan@fandm.edu>
2020-04-14 15:04:09 -0500

    install 'dotenv' to environmentall variables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 app.use(cors())
@@ -128,7 +130,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
