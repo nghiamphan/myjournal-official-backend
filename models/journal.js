01c9689e12b2560023cec3454e3211a575b96f1d
Nghia <nphan@fandm.edu>
2020-04-14 15:30:30 -0500

    use mongoose: create journal schema and a matching model; connect to MongoDB Atlas

---
+++ models/journal.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGO_URI
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('connect to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDb: ', error.message)
+	})
+
+const journalSchema = new mongoose.Schema({
+	date: Date,
+	todos: [],
+	reflection: String,
+	book_summaries: [],
+	words_of_today: []
+})
+
+journalSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+module.exports = mongoose.model('Journal', journalSchema)
