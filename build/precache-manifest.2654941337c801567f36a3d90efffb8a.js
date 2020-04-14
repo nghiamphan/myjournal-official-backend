5bee51c2396009754580fac82f1a4c5ceace2fcf
Nghia <nphan@fandm.edu>
2020-04-14 14:36:44 -0500

    fix mixing up build folder

---
+++ build/precache-manifest.2654941337c801567f36a3d90efffb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8941882c387326d09f3df964e4a829f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34efb1811cfcc2072871",
+    "url": "/static/css/main.247d6e65.chunk.css"
+  },
+  {
+    "revision": "71a1f2ad202a8e67a8e3",
+    "url": "/static/js/2.12e37acc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12e37acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34efb1811cfcc2072871",
+    "url": "/static/js/main.c54b8f86.chunk.js"
+  },
+  {
+    "revision": "295b245554715f2bc338",
+    "url": "/static/js/runtime-main.d03ab7a6.js"
+  }
+]);
---
+++ build/precache-manifest.b88369c8a416e0ebe091dd81d0ee7f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02905806b2f58d7853487f5dc7798174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e63b59aaa64b295fd10e",
+    "url": "/static/css/main.247d6e65.chunk.css"
+  },
+  {
+    "revision": "71a1f2ad202a8e67a8e3",
+    "url": "/static/js/2.12e37acc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12e37acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63b59aaa64b295fd10e",
+    "url": "/static/js/main.ced5c005.chunk.js"
+  },
+  {
+    "revision": "295b245554715f2bc338",
+    "url": "/static/js/runtime-main.d03ab7a6.js"
+  }
+]);
