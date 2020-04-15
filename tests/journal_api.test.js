110bdd62645ef3dd7dec990ba3d71e2387510880
Nghia <nphan@fandm.edu>
2020-04-15 09:07:32 -0500

    write some tests with jest and supertest

---
+++ tests/journal_api.test.js
@@ -0,0 +1,140 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const Journal = require('../models/journal')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+	await Journal.deleteMany({})
+
+	const journalObjects = helper.initialJournals
+		.map(journal => new Journal(journal))
+	const promiseArray = journalObjects.map(journal => journal.save())
+	await Promise.all(promiseArray)
+})
+
+describe('when there is initially some journals saved', () => {
+	test('journals are returned as json', async () => {
+		await api
+			.get('/api/journals')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all journals are returned', async () => {
+		const response = await api.get('/api/journals')
+
+		expect(response.body).toHaveLength(helper.initialJournals.length)
+	})
+
+	test('a specific journal is within the returned journals', async () => {
+		const response = await api.get('/api/journals')
+
+		const dates = response.body.map(r => r.date)
+		expect(dates).toContain('2020-03-23')
+
+		const reflections = response.body.map(r => r.reflection)
+		expect(reflections).toContain('Today is good.')
+	})
+
+	describe('viewing a specific journal', () => {
+		test('succeeds with a valid id', async () => {
+			const journalsAtStart = await helper.journalsInDb()
+
+			const journalToView = journalsAtStart[0]
+
+			const response = await api
+				.get(`/api/journals/${journalToView.id}`)
+				.expect(200)
+				.expect('Content-Type', /application\/json/)
+
+			expect(response.body).toEqual(journalToView.toJSON())
+		})
+
+		test('fails with statuscode 404 if note does not exist', async () => {
+			const validNonexistingId = await helper.nonExistingId()
+
+			await api
+				.get(`/api/journals/${validNonexistingId}`)
+				.expect(404)
+		})
+
+		test('fails with statuscode 400 id is invalid', async () => {
+			const invalidId = '5a3d5da59070081a82a3445'
+
+			await api
+				.get(`/api/journals/${invalidId}`)
+				.expect(400)
+		})
+	})
+
+	describe('addition of a new note', () => {
+		test('succeeds with valid data', async () => {
+			const newJournal = {
+				date: '2020-01-01',
+				todos: [],
+				reflection: 'add a new journal test',
+				book_summaries: [],
+				words_of_today: []
+			}
+
+			await api
+				.post('/api/journals')
+				.send(newJournal)
+				.expect(200)
+				.expect('Content-Type', /application\/json/)
+
+			const journalsAtEnd = await helper.journalsInDb()
+			expect(journalsAtEnd).toHaveLength(helper.initialJournals.length + 1)
+
+			const dates = journalsAtEnd.map(r => r.date)
+			expect(dates).toContain('2020-01-01')
+
+			const reflections = journalsAtEnd.map(r => r.reflection)
+			expect(reflections).toContain('add a new journal test')
+		})
+
+		test('fails with status code 400 if data invaild', async () => {
+			const newJournal = {
+				date: '2020-01-01',
+				todos: [],
+				book_summaries: [],
+				words_of_today: []
+			}
+
+			await api
+				.post('/api/journals')
+				.send(newJournal)
+				.expect(400)
+
+			const journalsAtEnd = await helper.journalsInDb()
+			expect(journalsAtEnd).toHaveLength(helper.initialJournals.length)
+		})
+	})
+
+	describe('deletion of a note', () => {
+		test('succeeds with status code 204 if id is valid', async () => {
+			const journalsAtStart = await helper.journalsInDb()
+			const journalToDelete = journalsAtStart[0]
+
+			await api
+				.delete(`/api/journals/${journalToDelete.id}`)
+				.expect(204)
+
+			const journalAtEnd = await helper.journalsInDb()
+			expect(journalAtEnd).toHaveLength(helper.initialJournals.length - 1)
+
+			const dates = journalAtEnd.map(r => r.date)
+			expect(dates).not.toContain('2020-03-23')
+
+			const reflections = journalAtEnd.map(r => r.reflection)
+			expect(reflections).not.toContain('Today is good.')
+		})
+	})
+})
+
+afterAll(() => {
+	mongoose.connection.close()
+})
---
+++ tests/test_helper.js
@@ -0,0 +1,80 @@
+const Journal = require('../models/journal')
+
+const initialJournals = [
+	{
+		date: '2020-03-23',
+		todos: [
+			{
+				id: 1,
+				task: 'Learn React for two hours',
+				done: true
+			},
+			{
+				id: 2,
+				task: 'Learn to skate for one hour',
+				done: false
+			}
+		],
+		reflection: 'Today is good.',
+		book_summaries: [
+			{
+				id: 1,
+				title: 'Catch-22',
+				chapter: 'The Texan',
+				summary: 'This chapter is very interesting'
+			}
+		],
+		words_of_today: [
+			{
+				id: 1,
+				word: 'Epiphany',
+				definition: 'A suddent moment of insight or realization.'
+			}
+		]
+	},
+	{
+		date: '2020-03-24',
+		todos: [
+			{
+				id: 1,
+				task: 'Mediatation for 5 minutes',
+				done: true
+			}
+		],
+		reflection: 'Today is fantastic.',
+		book_summaries: [
+			{
+				id: 1,
+				title: 'Selfish Gene',
+				chapter: '12',
+				summary: 'This chapter is very insightful.'
+			}
+		],
+		words_of_today: []
+	}
+]
+
+const nonExistingId = async () => {
+	const journal = new Journal({
+		date: '01/01/2020',
+		todos: [],
+		reflection: 'willremovethissoon',
+		book_summaries: [],
+		words_of_today: []
+	})
+	await journal.save()
+	await journal.remove()
+
+	return journal._id.toString()
+}
+
+const journalsInDb = async () => {
+	const journals = await Journal.find({})
+	return journals
+}
+
+module.exports = {
+	initialJournals,
+	nonExistingId,
+	journalsInDb
+}
