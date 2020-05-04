0781cbc89637955c53b6603d6f691363f76e3ce5
Nghia <nphan@fandm.edu>
2020-05-03 19:55:10 -0500

    update build from front end

---
+++ build/precache-manifest.882bcedce680fa5bc47d1af3b75680f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ce1a1d96edf730b3f3e72075432f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d2a7fd5d3d04317d6d5",
+    "url": "/static/css/main.21ab2569.chunk.css"
+  },
+  {
+    "revision": "d2afcc6f40e12bb31d5a",
+    "url": "/static/js/2.0ace8cc0.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.0ace8cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2a7fd5d3d04317d6d5",
+    "url": "/static/js/main.59a95610.chunk.js"
+  },
+  {
+    "revision": "295b245554715f2bc338",
+    "url": "/static/js/runtime-main.d03ab7a6.js"
+  },
+  {
+    "revision": "14bd004e73822d5ac7fa4c6f84f4b811",
+    "url": "/static/media/login-journal.14bd004e.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.025508b6537e7e82e5eabafbcc3db155.js"
+  "/precache-manifest.882bcedce680fa5bc47d1af3b75680f6.js"
 );
 
 self.addEventListener('message', (event) => {
